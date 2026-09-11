8ef61f167e964f942bcfa7006ec163c3729ddbdf
vjt4 <54646079+vjt4@users.noreply.github.com>
2026-09-11 09:17:59 -0500

    added minimap removal based on zoom level, removal based on split screen and options in options menu

--- mods/ReUI.Minimap/Main.lua
+++ mods/ReUI.Minimap/Main.lua
@@ -9,10 +9,50 @@ ReUI.Require
 }
 
 function Main(isReplay)
+    local controls
+    local autoHideActive = false
+    local restoreVisible = false
+    local mapZoomScale = 1
+
+    local options = ReUI.Options.Mods["ReUI.Minimap"]
+
+    local function ShouldHideMinimap()
+        if options.hideOnSplitScreen:Get() and GetCamera("WorldCamera2") then
+            return true
+        end
+
+        if options.hideAtCameraDistance:Get() then
+            local camera = GetCamera("WorldCamera")
+            local threshold = options.cameraDistanceThreshold:Get() * mapZoomScale
+            if camera and camera:GetZoom() >= threshold then
+                return true
+            end
+        end
+
+        return false
+    end
+
+    local function UpdateMinimapVisibility()
+        if not controls or not controls.displayGroup then
+            return
+        end
+
+        if ShouldHideMinimap() then
+            if not autoHideActive then
+                restoreVisible = not controls.displayGroup:IsHidden()
+                autoHideActive = true
+            end
+            controls.displayGroup:Hide()
+        elseif autoHideActive then
+            autoHideActive = false
+            controls.displayGroup:SetHidden(not restoreVisible)
+        end
+    end
+
     ReUI.Core.Hook("/lua/ui/game/minimap.lua", "CreateMinimap", function(field, module)
         return function(parent)
             field(parent)
-            local controls = module.controls
+            controls = module.controls
 
             local oldHandleEvent = controls.miniMap.HandleEvent
 
@@ -23,13 +63,14 @@ function Main(isReplay)
                 return oldHandleEvent(self, event)
             end
 
-            local options = ReUI.Options.Mods["ReUI.Minimap"]
-            options.allowZoom:Bind(function(opt)
-                controls.miniMap.isZoom = opt()
+            controls.miniMap.isZoom = options.allowZoom:Get()
+            options.allowZoom.OnChanged:Add(function(_, value)
+                controls.miniMap.isZoom = value
             end)
 
             local mapWidth = SessionGetScenarioInfo().size[1]
             local mapHeight = SessionGetScenarioInfo().size[2]
+            mapZoomScale = mapWidth / 512
             local areaData = Sync.NewPlayableArea
             if areaData then
                 mapWidth = areaData[3] - areaData[1]
@@ -42,6 +83,19 @@ function Main(isReplay)
                 local right = displayGroup.Right()
                 local width = right - left
                 displayGroup.Bottom:Set(mapHeight * width / mapWidth + top + 35)
+            end
+
+            import("/lua/ui/game/gamemain.lua").AddBeatFunction(UpdateMinimapVisibility, true)
+        end
+    end)
+
+    ReUI.Core.Hook("/lua/ui/game/minimap.lua", "ToggleMinimap", function(field)
+        return function()
+            field()
+
+            if controls and controls.displayGroup and autoHideActive then
+                restoreVisible = not controls.displayGroup:IsHidden()
+                controls.displayGroup:Hide()
             end
         end
     end)
--- mods/ReUI.Minimap/Options.lua
+++ mods/ReUI.Minimap/Options.lua
@@ -1,6 +1,9 @@
-local Opt = ReUI.Options.Opt
+local OptionValue = ReUI.Options.OptionValue
 ReUI.Options.Mods["ReUI.Minimap"] = {
-    allowZoom = Opt(false)
+    allowZoom = OptionValue(false),
+    hideOnSplitScreen = OptionValue(false),
+    hideAtCameraDistance = OptionValue(false),
+    cameraDistanceThreshold = OptionValue(520),
 }
 
 function Main(isReplay)
@@ -9,6 +12,9 @@ function Main(isReplay)
 
     Options.AddOptions("ReUI.Minimap", "ReUI.Minimap", {
         Options.Filter("Allow zoom", options.allowZoom, 4),
+        Options.Filter("Hide in split-screen", options.hideOnSplitScreen, 4),
+        Options.Filter("Hide at camera distance", options.hideAtCameraDistance, 4),
+        Options.Slider("Camera distance threshold (10 km map)", 100, 1000, 10, options.cameraDistanceThreshold, 4),
     })
 
 end
